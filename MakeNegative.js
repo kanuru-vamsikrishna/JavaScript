7daf39b08eb4a2e8586918f66a1806f369217d59
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-01-21 15:17:58 +0530

    Make the given number negative

---
+++ MakeNegative.js
@@ -0,0 +1,16 @@
+/*
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+*/
+
+function makeNegative(num) {
+  if (num === 0) {
+    return 0
+  } else if (num < 0) {
+    return num
+  } else {
+    return -num
+  }
+}
+
+const result = makeNegative(-9)
+console.log("Result:", result)
